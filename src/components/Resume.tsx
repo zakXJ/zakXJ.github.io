cbaafa79bf5245458110da6055873d2543408e12
zakXJ <xj3.zak@gmail.com>
2025-03-21 13:44:06 +0100

    fix client load

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,7 +1,11 @@
-function Resume() {
+import { useState, useEffect } from "react";
 
-  const url = window.location.href;
-  console.log(url.includes('dz'));
+function Resume() {
+  const [currentUrl, setCurrentUrl] = useState('');
+  
+  useEffect(() => {
+    setCurrentUrl(window.location.href);
+  }, []);
 
   return (
     <section id="resume" className=" w-4/5 mx-auto bg-sk flex flex-col py-40 justify-center text-zinc-900">
@@ -11,14 +15,14 @@ function Resume() {
         </div>
         <div className="flex-1 flex sm:justify-end justify-center mt-2 sm:mt-0">
             <a
-            href={ url.includes('dz') ? "https://drive.google.com/file/d/1XQBBAhwHzR7sMBbgebxhIoR-SpcobOGM/view?usp=sharing" : "https://drive.google.com/file/d/1UA0tjdh4eRdTBJKmObySYtCoyIOMND07/view?usp=sharing"}
+            href={ currentUrl.includes('dz') ? "https://drive.google.com/file/d/1XQBBAhwHzR7sMBbgebxhIoR-SpcobOGM/view?usp=sharing" : "https://drive.google.com/file/d/1UA0tjdh4eRdTBJKmObySYtCoyIOMND07/view?usp=sharing"}
             className="relative w-fit bg-sky-300 p-4 rounded-3xl group cursor-pointer"
             target="_blank"
             rel="noopener noreferrer"
             >
             <div className="absolute top-4 rotate-0 bg-white aspect-[1.65/2] w-32 rounded-xl border border-zinc-200"></div>
             <div className="group-hover:-translate-y-10 transition-transform duration-500 ease-in-out rotate-[5deg] overflow-clip  z-10 shadow-lg shadow-zinc-800  bg-white aspect-[1.65/2] w-32 rounded-xl border border-zinc-200">
-              <img src={ url.includes('dz') ? `../cv2025DZ.png` : `../cv20252.png` } className="w-fit" alt="" />
+              <img src={ currentUrl.includes('dz') ? `../cv2025DZ.png` : `../cv20252.png` } className="w-fit" alt="" />
             </div>
 
             <div className=" inset-shadow-md inset-shadow-white bg-linear-to-t from-blue-300/90 to-white/90 folder w-full h-32 rounded-3xl absolute left-0 bottom-0"></div>
