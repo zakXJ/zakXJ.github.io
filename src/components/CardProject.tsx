b98654f6ea0e07f81427e498677c1194c811f872
zakXJ <xj3.zak@gmail.com>
2025-06-07 17:27:57 +0100

    refactor: enhance CardProject component with motion effects

--- src/components/CardProject.tsx
+++ src/components/CardProject.tsx
@@ -1,22 +1,30 @@
 import clsx from "clsx";
-
+import * as motion from "motion/react-client";
 interface CardProjectProps {
   img: string;
   logo: string;
   color?: string;
   width?: string;
+  rotation?: number;
 }
 
-function CardProject({ img, logo, color, width }: CardProjectProps) {
+function CardProject({ img, logo, color, width, rotation }: CardProjectProps) {
   return (
-    <div
+    <motion.div
+      whileHover={{ scale: 1.1, rotate: rotation ?? 1.5 }}
+      transition={{ 
+        type: "spring", 
+        stiffness: 400, 
+        damping: 25,
+        duration: 0.15 
+      }}
       className={clsx(
-        "rounded-3xl sm:w-1/3 p-4 border border-zinc-800",
+        "rounded-3xl sm:w-1/3 p-4 border border-zinc-800 isolate",
         color || "bg-zinc-100"
       )}
     >
       <div className="relative  overflow-clip rounded-xl border border-zinc-800">
-        <span className="absolute inset-0 bg-white/70 backdrop-blur-lg mask-gradient"></span>
+        <span className="absolute inset-0 bg-white backdrop-blur-lg imask-gradient mask-alpha mask-r-from-black mask-r-from-50% mask-r-to-transparent"></span>
         <img
           src={`../images/projects/${img}`}
           className="w-fit object-cover"
@@ -37,7 +45,7 @@ function CardProject({ img, logo, color, width }: CardProjectProps) {
           See Projects
         </button>
       </div> */}
-    </div>
+    </motion.div>
   );
 }
 
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,14 +1,14 @@
 import CardProject from "./CardProject";
 
 const projects = [
-    { img: 'shero.png', logo: 'shero-logo.svg', color:'bg-yellow-800/20' },
-    { img: 'mmik.png', logo: 'mmik-logo.svg', color:'bg-sky-600/20' },
-    { img: 'binary01.png', logo: 'binary01-logo.svg', color:'bg-purple-600/20', width: 'w-1/3' },
+    { img: 'shero.png', logo: 'shero-logo.svg', color:'bg-yellow-100' },
+    { img: 'mmik.png', logo: 'mmik-logo.svg', color:'bg-sky-200', rotation: -1.2 },
+    { img: 'binary01.png', logo: 'binary01-logo.svg', color:'bg-purple-200', width: 'w-1/3', rotation: 1.2 },
   ];
 
 function Projects() {
     const listProjects = projects.map(project =>
-        <CardProject img={project.img} logo={project.logo} color={project.color} width={project.width}/>
+        <CardProject img={project.img} logo={project.logo} color={project.color} width={project.width} rotation={project.rotation}/>
       );
   return (
     <section id="projects" className=" w-4/5 mx-auto flex flex-col py-36 justify-center text-zinc-900">
